8c7caff6636b5ec797ef7e5fe5c4c1e2d4d6799b
Zhao Zhenlong <zhaozhenlong.dev@gmail.com>
2018-12-06 13:08:33 +0800

    remove fight feed upgrade in sol contract

--- contracts/ThingCore.sol
+++ contracts/ThingCore.sol
@@ -90,11 +90,8 @@ contract ThingFactory is Ownable {
         string name;       // 名字
         uint price;        // 价格
         uint dna;          // DNA
-        uint32 level;      // 等级
         uint32 readyTime;  // 技能冷却
         uint32 generation; // 代数
-        uint16 winCount;   // 战斗胜利次数
-        uint16 lossCount;  // 战斗失败次数
     }
 
     Thing[] public things;
@@ -112,11 +109,8 @@ contract ThingFactory is Ownable {
         _thing.name = _name;
         _thing.price = (_dna / 100) % 100;
         _thing.dna = _dna;
-        _thing.level = uint32(1);
         _thing.readyTime = uint32(now);
         _thing.generation = _generation;
-        _thing.winCount = uint16(0);
-        _thing.lossCount = uint16(0);
 
         // 记录到区块链
         uint id = things.push(_thing) - 1;
@@ -142,7 +136,7 @@ contract ThingFactory is Ownable {
 }
 
 /// 繁育&喂养系统
-contract ThingFeedAndBreed is ThingFactory {
+contract ThingBreed is ThingFactory {
 
     modifier onlyOwnerOf(uint _thingId) {
         require(msg.sender == thingToOwner[_thingId]);
@@ -174,100 +168,24 @@ contract ThingFeedAndBreed is ThingFactory {
         return (_thing.readyTime <= now);
     }
 
-    function feedAndMultiply(uint _thingId, uint _targetDna, string _species) internal onlyOwnerOf(_thingId) {
+    function multiply(uint _thingId, uint _targetDna) internal onlyOwnerOf(_thingId) {
         Thing storage myThing = things[_thingId];
         require(_isReady(myThing));
         _targetDna = _targetDna % dnaModulus;
         uint newDna = (myThing.dna + _targetDna) / 2;
-        // 吃了Kitty后，dna最后两个数字设定为99
-        // 例如：7290459416715799
-        if (keccak256(_species) == keccak256("kitty")) {
-            newDna = newDna - newDna % 100 + 99;
-        }
         _createThing(strConcat("n",myThing.name, "", "", ""), newDna, myThing.generation + 1);
         _triggerCooldown(myThing);
-    }
-
-    // 喂养
-    function feedOnKitty(uint _thingId, uint _kittyId) public {
-        // 使用_kittyId作为kittyDna
-        uint kittyDna = _kittyId;
-        // (,,,,,,,,,kittyDna) = kittyContract.getKitty(_kittyId);
-        feedAndMultiply(_thingId, kittyDna, "kitty");
     }
 
     // 繁育
     function breed(uint _thingId, uint _targetThingId) public {
         uint _targetDna = things[_targetThingId].dna;
-        feedAndMultiply(_thingId, _targetDna, "thing");
-    }
-}
-
-/// 升级系统
-contract ThingUpgrade is ThingFeedAndBreed {
-
-    // 默认升级费用 1 ETH
-    uint levelUpFee = 1 ether;
-
-    modifier aboveLevel(uint _level, uint _thingId) {
-        require(things[_thingId].level >= _level);
-        _;
-    }
-
-    // 设置升级费用
-    function setLevelUpFee(uint _fee) external onlyOwner {
-        levelUpFee = _fee;
-    }
-
-    // 升级
-    function levelUp(uint _thingId) external payable {
-        require(msg.value == levelUpFee);
-        things[_thingId].level++;
-    }
-
-    // 2级可以改名
-    function changeName(uint _thingId, string _newName) external aboveLevel(2, _thingId) onlyOwnerOf(_thingId) {
-        things[_thingId].name = _newName;
-    }
-
-    // 20级可以定制Dna
-    function changeDna(uint _thingId, uint _newDna) external aboveLevel(20, _thingId) onlyOwnerOf(_thingId) {
-        things[_thingId].dna = _newDna;
-    }
-}
-
-/// 对战系统
-contract ThingAttack is ThingUpgrade {
-
-    uint randNonce = 0;
-    // 攻击方将有70%的几率获胜，防守方将有30%的几率获胜
-    uint attackVictoryProbability = 70;
-
-    function randMod(uint _modulus) internal returns(uint) {
-        randNonce++;
-        return uint(keccak256(now, msg.sender, randNonce)) % _modulus;
-    }
-
-    // 两个产品互相对战
-    function attack(uint _thingId, uint _targetId) external onlyOwnerOf(_thingId) {
-        Thing storage myThing = things[_thingId];
-        Thing storage enemyThing = things[_targetId];
-        uint rand = randMod(100);
-        if (rand <= attackVictoryProbability) {
-            myThing.winCount++;
-            myThing.level++;
-            enemyThing.lossCount++;
-            feedAndMultiply(_thingId, enemyThing.dna, "thing");
-        } else {
-            myThing.lossCount++;
-            enemyThing.winCount++;
-            _triggerCooldown(myThing);
-        }
+        multiply(_thingId, _targetDna);
     }
 }
 
 /// 辅助合约
-contract ThingHelper is ThingAttack {
+contract ThingHelper is ThingBreed {
 
     function withdraw() external onlyOwner {
         owner.transfer(this.balance);
@@ -291,20 +209,14 @@ contract ThingHelper is ThingAttack {
         string name,
         uint price,
         uint dna,
-        uint32 level,
         uint32 readyTime,
-        uint32 generation,
-        uint16 winCount,
-        uint16 lossCount) {
+        uint32 generation) {
         Thing storage thing = things[_thingId];
         name = thing.name;
         price = thing.price;
         dna = thing.dna;
-        level = thing.level;
         readyTime = thing.readyTime;
         generation = thing.generation;
-        winCount = thing.winCount;
-        lossCount = thing.lossCount;
     }
 }
 
