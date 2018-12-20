dca669de5d8e13a5fa733fd1ea720c2cc69d0591
1363720314@qq.com <1363720314@qq.com>
2018-12-20 19:19:20 +0800

    fix server.js statics

--- src/server.js
+++ src/server.js
@@ -1,8 +1,9 @@
 let express = require('express');
 let app = express();
 
-app.use(express.static("./"));
 
+app.use(express.static('./'));
+app.use('/build',express.static('../build'));
 app.listen(3000, function() {
     console.log("Dapp server start on port: 3000");
 });
